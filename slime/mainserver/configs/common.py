c3ab1620a89c3b205fe965c56b2c758c2ec84f6f
pakoo <zealzpc@gmail.com>
2013-01-07 01:29:12 -0500

    add configs

---
+++ slime/mainserver/configs/common.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+db_para={
+        'host':'localhost',
+        'port':27017,
+}
+
+cache_para= {
+        'host':'localhost',
+        'port':6379,
+        'db':1,
+}
+
+
+if __name__ == '__main__':
+    pass
